31a91b298e422922da7fb3234107e8ac5c46736b
freelancernizamc <nizamuddin2020ctg@gmail.com>
2024-01-05 23:47:03 +0600

    web development page completed

---
+++ src/pages/WebDevelopment/WdText/WdText.jsx
@@ -0,0 +1,38 @@
+const WdText = () => {
+  return (
+    <div className="bg-[#07121C] p-5 text-white">
+      <div className="text-justify py-10 px-4 md:px-8 lg:px-20 rounded-md shadow-2xl pt-8">
+        <h2 className="text-lg md:text-xl lg:text-2xl font-bold pb-5">
+          Custom Web Development Services Tailored for Your Business
+        </h2>
+        <p>
+          we specialize in crafting stunning, functional, and user-friendly
+          websites that drive results for your business. Our Custom Web
+          Development Services are designed to help you stand out in the digital
+          landscape, captivate your audience, and achieve your online goals.
+          Whether you’re a startup looking to establish a strong online
+          presence, a growing business seeking to enhance user engagement, or an
+          established enterprise striving to stay ahead of the competition.
+        </p>
+        <p>
+          Ossisto has the expertise and experience to deliver exceptional
+          website development services tailored to your unique needs. With a
+          focus on innovative design, responsive functionality, and seamless
+          user experiences, we create websites that not only look stunning but
+          also drive results. Let us be your digital partner in crafting a
+          custom web development services that sets you apart. Explore our
+          comprehensive range of services and let’s embark on a journey to
+          digital success together.
+        </p>
+        <div className="pt-10">
+          <h3>WHY TO CHOOSE US</h3>
+          <h2 className="text-xl font-semibold">
+            EXPERIENCE EXCELLENCE IN WEB DEVELOPMENT
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WdText;
--- src/pages/WebDevelopment/WebDevelopment.jsx
+++ src/pages/WebDevelopment/WebDevelopment.jsx
@@ -1,5 +1,39 @@
+import { Helmet } from "react-helmet-async";
+import background from "../../assets/services/banner-background.png";
+import gradientOverlay from "../../assets/images/baygrond.png";
+import WdBanner from "./WdBanner/WdBanner";
+import WdText from "./WdText/WdText";
+import WdFeature from "./WdFeature/WdFeature";
+
 const WebDevelopment = () => {
-  return <div>Web Development</div>;
+  return (
+    <div>
+      <Helmet>
+        <title>Intela ~ Services ~ Web Development</title>
+      </Helmet>
+      <div
+        className="relative bg-cover h-screen"
+        style={{
+          backgroundImage: `url(${background})`,
+          height: "100vh",
+        }}
+      >
+        <div
+          className="absolute inset-0 flex items-center justify-center"
+          style={{
+            backgroundImage: `url(${gradientOverlay})`,
+            opacity: 0.97,
+          }}
+        >
+          <div className="text-white text-center">
+            <WdBanner />
+          </div>
+        </div>
+      </div>
+      <WdText />
+      <WdFeature />
+    </div>
+  );
 };
 
 export default WebDevelopment;
