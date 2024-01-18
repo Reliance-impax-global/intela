7e9d833f45e00fc2248a7ee476e1d668e2121c11
freelancernizamc <nizamuddin2020ctg@gmail.com>
2024-01-18 20:46:06 +0600

    Contact page updated

--- src/pages/Contact/ContactBanner/ContactBanner.jsx
+++ src/pages/Contact/ContactBanner/ContactBanner.jsx
@@ -2,14 +2,21 @@ import contactBanner from "../../../assets/contact/contactImg.png";
 
 const ContactBanner = () => {
   return (
-    <div className="relative bg-[#14150C] z-30 mx-4 md:mx-5 lg:mx-10 border-none rounded-2xl">
-      <h1 className="text-2xl text-left font-bold">Contact Us</h1>
-      <div className="md:w-full mx-0 md:mx-0">
-        <img
-          src={contactBanner}
-          alt="banner-image"
-          className="w-full h-full object-fill opacity-40"
-        />
+    <div className="relative z-30 ml-20 md:mt-24 pb-12 lg:ml-28 border-none rounded-2xl">
+      <h1
+        className="text-left md:py-5 ml-12"
+        style={{ fontFamily: "Antic", fontWeight: 400, fontSize: "48px" }}
+      >
+        contact us
+      </h1>
+      <div className="ml-10">
+        <div className="md:w-full">
+          <img
+            src={contactBanner}
+            alt="banner-image"
+            className="max-w-full md:w-8/12 h-full object-fill"
+          />
+        </div>
       </div>
     </div>
   );
--- src/pages/Contact/ContactForm/ContactForm.jsx
+++ src/pages/Contact/ContactForm/ContactForm.jsx
@@ -4,18 +4,16 @@ import envelope from "../../../assets/contact/envelope 1.png";
 
 const ContactForm = () => {
   return (
-    <div className="bg-[#021D4A] text-white p-20">
-      <div className="md:flex md:justify-around">
-        <div className="sm:w-full md:w-1/3 lg:w-1/3 bg-[#082C6B] rounded-lg mb-8 md:mb-0 pb-2">
-          <h2 className="mb-5 mt-10 pt-2 text-xl font-bold ml-10">
-            Get in touch
-          </h2>
+    <div className="bg-[#021D4A] text-white md:p-20 p-10">
+      <div className="md:flex md:justify-center gap-12 p-5">
+        <div className="w-full md:w-2/3 lg:w-1/3 md:mr-5 md:mt-5 py-2 lg:mr-0 pt-5 bg-[#082C6B] rounded-lg sm:py-4">
+          <h2 className="mb-10 mt-2 text-xl font-bold ml-10">Get in touch</h2>
           <p className="ml-10 text-sm pr-5">
             Use our contact form for all information requests or contact us
             directly using the contact information below. Feel free to get in
             touch with us via email or phone
           </p>
-          <div className="flex  ml-10 mt-8">
+          <div className="flex  ml-8 mt-8">
             <img className="w-[20px] h-[20px]" src={location} alt="Location" />
             <div>
               <p className="text-sm ml-2">OUR OFFICE LOCATION</p>
@@ -40,11 +38,11 @@ const ContactForm = () => {
             </div>
           </div>
         </div>
-        <div className="sm:w-full md:w-1/3 lg:w-1/3 bg-[#082C6B] rounded-lg">
+        <div className="w-full md:w-2/3 lg:w-1/3 bg-[#082C6B] rounded-lg sm:mt-3 sm:pt-1 px-2 lg:pr-4">
           <h2 className="mb-5 mt-10 text-xl font-bold ml-10">
             HOW WE CAN ASSIST YOU{" "}
           </h2>
-          <div className="md:ml-10">
+          <div className="md:ml-6">
             <form>
               <input
                 type="text"
