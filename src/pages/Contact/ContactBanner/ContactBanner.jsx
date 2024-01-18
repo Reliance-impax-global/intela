b56650923ff16ec9e46c2cf27cee70c9292db883
freelancernizamc <nizamuddin2020ctg@gmail.com>
2024-01-18 22:34:42 +0600

    Update code for responsiveness added in contact page

--- src/pages/Contact/ContactBanner/ContactBanner.jsx
+++ src/pages/Contact/ContactBanner/ContactBanner.jsx
@@ -2,14 +2,14 @@ import contactBanner from "../../../assets/contact/contactImg.png";
 
 const ContactBanner = () => {
   return (
-    <div className="relative z-30 ml-20 md:mt-24 pb-12 lg:ml-28 border-none rounded-2xl">
+    <div className="relative z-30 md:ml-20 md:mt-24 pb-12 lg:ml-28 border-none rounded-2xl">
       <h1
         className="text-left md:py-5 ml-12"
         style={{ fontFamily: "Antic", fontWeight: 400, fontSize: "48px" }}
       >
         contact us
       </h1>
-      <div className="ml-10">
+      <div className="md:ml-10">
         <div className="md:w-full">
           <img
             src={contactBanner}
